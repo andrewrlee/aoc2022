04282097ce515ec07972241a2dc4a9580590b402
andrew lee <andrewlee@andrews-air.home>
2022-12-03 12:52:00 +0000

    Adding new day scaffolding

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # AoC 2022
 
 with a JFDI attitude, I just want to taste to those sweet, sweet stars. 
-Currently in node.
+
+Currently using node/typescript.
 
 ## Testing
 
 use `npm test` to run all tests
 
-use `npm start -- day03` to run jest in watch mode for a single days solution
+use e.g: `npm start -- day01` to run jest in watch mode for a single days solution
+
+use `npm run add-day` to scaffold a new days challenge
---
+++ plopfile.js
@@ -0,0 +1,28 @@
+import fs from 'fs'
+
+export default function (plop) {
+
+	const completedChallenges = fs.readdirSync('.')
+	.filter(d => d.startsWith('day'))
+	.map(d => d.replace( /^\D+/g, ''))
+	.map(d => parseInt(d))
+
+	const day = (Math.max(...completedChallenges) + 1).toString().padStart(2, '0')
+	console.log(`Scaffolding day: ${day}`) 
+
+	plop.setGenerator('newDay', {
+		description: 'A way of adding a new day', 
+		prompts: [],
+		actions: [{
+			type: 'add',
+			path: `day${day}/input.txt`,
+			templateFile: 'templates/input.txt',
+		},{
+			type: 'add',
+			path: `day${day}/test.ts`,
+			templateFile: 'templates/test.ts',
+			data: { day: day}
+		}
+	]  
+	});
+};
