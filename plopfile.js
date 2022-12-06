14c8feaa6510412d64cf481adea2d1ccfbd43f7c
Andrew Lee <plasma147@gmail.com>
2022-12-06 20:34:52 +0000

    Adding mechanism for auto downloading input

--- plopfile.js
+++ plopfile.js
@@ -1,15 +1,26 @@
 import fs from 'fs'
+import * as dotenv from 'dotenv' 
+dotenv.config()
 
-export default function (plop) {
-
+export default async function (plop) {
+	const session = process.env.SESSION
+	
 	const completedChallenges = fs.readdirSync('.')
 	.filter(d => d.startsWith('day'))
 	.map(d => d.replace( /^\D+/g, ''))
 	.map(d => parseInt(d))
 
-	const day = (Math.max(...completedChallenges) + 1).toString().padStart(2, '0')
+	const rawDate = (Math.max(...completedChallenges) + 1).toString()
+	const day = rawDate.padStart(2, '0')
+	
 	console.log(`Scaffolding day: ${day}`) 
-
+	
+	let input = "Provide input manually"
+	if (session) {
+		const result = await fetch(`https://adventofcode.com/2022/day/${rawDate}/input`, { headers: { 'cookie' : 'session=53616c7465645f5f2b5bb3376e36bf319cebb2f1e3b40445e803ba46b90a794f53e48a515d145addb05a774d7dcef661a76b1e3cfbb31100ed762b8c887d95d5' }})
+		input = await result.text()	
+	} 
+	
 	plop.setGenerator('newDay', {
 		description: 'A way of adding a new day', 
 		prompts: [],
@@ -17,6 +28,7 @@ export default function (plop) {
 			type: 'add',
 			path: `day${day}/input.txt`,
 			templateFile: 'templates/input.txt',
+			data: { input }
 		},{
 			type: 'add',
 			path: `day${day}/challenge.test.ts`,
