a3a5cd2cc5feab4ea457a08c07dc6f1d6db616a9
Andrew Lee <plasma147@gmail.com>
2022-12-11 14:33:02 +0000

    Restructuring and making room for part2

--- day11/challenge.test.ts
+++ day11/challenge.test.ts
@@ -1,8 +1,12 @@
 import fs from "fs";
 
+type MonkeyMap = Map<string, Monkey>;
+
 class Monkey {
   id: string;
   items: number[];
+  divisor: number;
+
   operationStmt: string;
   testStmt: string;
   activity: number = 0;
@@ -16,6 +20,9 @@ class Monkey {
     this.operationStmt = /\s+Operation: (.*)/
       .exec(this.content[2])[1]
       .replaceAll(/\w{3}/g, (w) => w.substring(0, 1));
+
+    const [, d] = /Test: divisible by (\d+)/.exec(content[3]);
+    this.divisor = parseInt(d);
 
     this.testStmt = [content[3], content[4], content[5]]
       .join(" ")
@@ -40,7 +47,23 @@ class Monkey {
     this.items = this.items.concat(items);
   }
 
-  doTurn(): Map<string, number[]> {
+  turnAssignments(): Map<string, number[]> {
+    this.activity += this.items.length;
+    let r = this.items.reduce((results, item) => {
+      let newWorryValue = Math.floor(this.execute(item) / 3);
+      let destination = this.testVal(newWorryValue);
+      let itemsForMonkeys = [
+        ...(results.get(destination) || []),
+        newWorryValue,
+      ];
+      results.set(destination, itemsForMonkeys);
+      return results;
+    }, new Map<string, number[]>());
+    this.items = [];
+    return r;
+  }
+
+  turnAssignmentsForPart2(cd: number): Map<string, number[]> {
     this.activity += this.items.length;
     let r = this.items.reduce((results, item) => {
       let newWorryValue = Math.floor(this.execute(item) / 3);
@@ -57,42 +80,53 @@ class Monkey {
   }
 }
 
-const monkeys = fs
-  .readFileSync(`${__dirname}/input.txt`)
-  .toString()
-  .split("\n\n")
-  .filter((s) => s)
-  .map((s) => new Monkey(s.split("\n")));
-const monkeyMap = Object.fromEntries(monkeys.map((m) => [m.id, m]));
+const createMonkeyMap = (): MonkeyMap =>
+  fs
+    .readFileSync(`${__dirname}/input.txt`)
+    .toString()
+    .split("\n\n")
+    .filter((s) => s)
+    .map((s) => new Monkey(s.split("\n")))
+    .reduce((acc, m) => {
+      acc.set(m.id, m);
+      return acc;
+    }, new Map<string, Monkey>());
 
-const doRound = () => {
-  monkeys.forEach((monkey) => {
-    const assignments = monkey.doTurn();
-    assignments.forEach((items, m) => {
-      monkeyMap[m].addItems(items);
-    });
-  });
+const calculateMonkeyBusiness = (monkeys: MonkeyMap) => {
+  const [first, second] = Array.from(monkeys.values())
+    .map((m) => m.activity)
+    .sort((a, b) => b - a);
+  return first * second;
 };
 
 describe("day11", () => {
   test("answer1", () => {
+    const monkeys = createMonkeyMap();
     for (let i = 0; i < 20; i++) {
-      doRound();
+      monkeys.forEach((monkey) => {
+        monkey.turnAssignments().forEach((items, m) => {
+          monkeys.get(m).addItems(items);
+        });
+      });
     }
 
-    const [first, second] = Object.values(monkeyMap)
-      .map((m) => m.activity)
-      .sort((a, b) => b - a);
-
-    expect(first * second).toStrictEqual(88208);
+    expect(calculateMonkeyBusiness(monkeys)).toStrictEqual(88208);
   });
 
   test("answer2", () => {
-    const answer = -1;
-    expect(answer).toStrictEqual(1);
-  });
+    const monkeys = createMonkeyMap();
+    const cd = Array.from(monkeys.values()).reduce(
+      (acc, m) => acc * m.divisor,
+      1
+    );
+    for (let i = 0; i < 20; i++) {
+      monkeys.forEach((monkey) => {
+        monkey.turnAssignmentsForPart2(cd).forEach((items, m) => {
+          monkeys.get(m).addItems(items);
+        });
+      });
+    }
 
-  test("other", () => {
-    expect(true).toBe(true);
+    expect(calculateMonkeyBusiness(monkeys)).toStrictEqual(88208);
   });
 });
