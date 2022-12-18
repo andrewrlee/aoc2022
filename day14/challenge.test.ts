7875836d33bca01d33e7f5d064ca798184ae4b9d
Andrew Lee <plasma147@gmail.com>
2022-12-18 11:06:22 +0000

    Fix issue with handlebars escaping html by default

---
+++ day14/challenge.test.ts
@@ -0,0 +1,152 @@
+import fs from "fs";
+
+type Coordinate = [x: number, y: number];
+type CoordWithVal<T> = { x: number; y: number; value: T };
+type Grid<T> = CoordWithVal<T>[][];
+
+const slidingWindow = <T>(items: T[]): [first: T, second: T][] => {
+  const result: [first: T, second: T][] = [];
+  for (let i = 1; i < items.length; i++) {
+    result.push([items[i - 1], items[i]]);
+  }
+  return result;
+};
+
+const fillIn = <T>(start: Coordinate, end: Coordinate): Coordinate[] => {
+  const [endX, endY] = end;
+  let [currentX, currentY] = start;
+  const xMod = Math.sign(endX - currentX);
+  const yMod = Math.sign(endY - currentY);
+  const result: Coordinate[] = [start];
+  while (currentX !== endX || currentY !== endY) {
+    result.push([(currentX += xMod), (currentY += yMod)]);
+  }
+  return result;
+};
+
+const walls = fs
+  .readFileSync(`${__dirname}/input.txt`)
+  .toString()
+  .split("\n")
+  .filter((s) => s)
+  .map((row) => {
+    const coords = row
+      .split(" -> ")
+      .map((c) => c.split(",").map((a) => parseInt(a)) as Coordinate);
+    return slidingWindow(coords).map(([start, end]) => fillIn(start, end));
+  })
+  .flat(2);
+
+const maxX = Math.max(...walls.map(([x, y]) => x));
+const maxY = Math.max(...walls.map(([x, y]) => y));
+const minX = Math.min(...walls.map(([x, y]) => x));
+const minY = Math.min(...walls.map(([x, y]) => y));
+
+const createGrid = <T>([maxX, maxY]: Coordinate, defaultValue: T): Grid<T> => {
+  const rows: CoordWithVal<T>[][] = [];
+  for (let y = 0; y < maxY + 1; y++) {
+    const row: CoordWithVal<T>[] = [];
+    for (let x = 0; x < maxX + 1; x++) {
+      row.push({ x, y, value: defaultValue });
+    }
+    rows.push(row);
+  }
+  return rows;
+};
+
+const rotate = <T>(arr: T[][]) =>
+  arr[0].map((_, i) => arr.map((r) => r[i]).reverse());
+
+const populateGrid = <T>(grid: Grid<T>, values: Coordinate[], fillValue: T) => {
+  values.forEach(([x, y]) => {
+    grid[y][x].value = fillValue;
+  });
+};
+
+const tracePath = (grid: Grid<string>, [entryX, y]: Coordinate) => {
+  const rGrid = rotate(grid);
+
+  const findFirstPoint = () => findDownFrom(entryX);
+  const findDownFrom = (x: number, xMin: number = 0) => {
+    const a = [...rGrid[x]].reverse().slice(xMin);
+    const i = a.findIndex(({ value }) => value !== ".");
+    return a.at(i - 1);
+  };
+
+  const findNextPoint = (
+    current: CoordWithVal<string>
+  ): CoordWithVal<string> => {
+    if (!current) return undefined;
+    const { x, y } = current;
+    const left = grid[y + 1]?.[x - 1];
+    const below = grid[y + 1]?.[x];
+    const right = grid[y + 1]?.[x + 1];
+    if (y + 1 > maxY) return undefined;
+    if (left?.value === ".") return findNextPoint(findDownFrom(left.x, y + 1));
+    if (right?.value === ".")
+      return findNextPoint(findDownFrom(right.x, y + 1));
+    if (below?.value === ".") return findNextPoint(below);
+    else return current;
+  };
+
+  for (let i = 0; i < Number.MAX_SAFE_INTEGER; i++) {
+    const currentPoint = findNextPoint(findFirstPoint());
+    if (!currentPoint) {
+      return i;
+    }
+    currentPoint.value = "o";
+  }
+};
+
+describe("day14", () => {
+  test("answer1", () => {
+    const grid = createGrid([maxX, maxY], ".");
+    populateGrid(grid, walls, "#");
+    const answer = tracePath(grid, [500, 0]);
+
+    expect(answer).toStrictEqual(1068);
+  });
+
+  const tracePath2 = (grid: Grid<string>, [entryX, y]: Coordinate) => {
+    const rGrid = rotate(grid);
+    const findFirstPoint = () => findDownFrom(entryX);
+    const findDownFrom = (x: number, xMin: number = 0) => {
+      const a = [...rGrid[x]].reverse().slice(xMin);
+      const i = a.findIndex(({ value }) => value !== ".");
+      return a.at(i - 1);
+    };
+
+    const findNextPoint = (
+      current: CoordWithVal<string>
+    ): CoordWithVal<string> => {
+      if (!current) return undefined;
+      const { x, y } = current;
+      const left = grid[y + 1]?.[x - 1];
+      const below = grid[y + 1]?.[x];
+      const right = grid[y + 1]?.[x + 1];
+      if (left?.value === ".")
+        return findNextPoint(findDownFrom(left.x, y + 1));
+      if (right?.value === ".")
+        return findNextPoint(findDownFrom(right.x, y + 1));
+      if (below?.value === ".") return findNextPoint(below);
+      else return current;
+    };
+
+    for (let i = 0; i < Number.MAX_SAFE_INTEGER; i++) {
+      if (grid[0][500].value === "o") {
+        return i;
+      }
+      const currentPoint = findNextPoint(findFirstPoint());
+      currentPoint.value = "o";
+    }
+  };
+
+  test("answer2", () => {
+    const grid = createGrid([maxX + 500, maxY + 2], ".");
+    const floor = fillIn([0, maxY + 2], [maxX + 100, maxY + 2]);
+    populateGrid(grid, [...walls, ...floor], "#");
+
+    const answer = tracePath2(grid, [500, 0]);
+    expect(answer).toStrictEqual(27936);
+  });
+});
--- plopfile.js
+++ plopfile.js
@@ -14,7 +14,7 @@ export default async function (plop) {
 	const day = rawDate.padStart(2, '0')
 	
 	console.log(`Scaffolding day: ${day}`) 
-	
+
 	let input = "Provide input manually"
 	if (session) {
 		const result = await fetch(`https://adventofcode.com/2022/day/${rawDate}/input`, { headers: { 'cookie' : 'session=53616c7465645f5f2b5bb3376e36bf319cebb2f1e3b40445e803ba46b90a794f53e48a515d145addb05a774d7dcef661a76b1e3cfbb31100ed762b8c887d95d5' }})
